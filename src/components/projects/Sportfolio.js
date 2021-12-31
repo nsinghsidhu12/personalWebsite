0f68c6f747d19baa693f4b3b9fb3aa37796ea1c4
nsinghsidhu12 <nsinghsidhu12@gmail.com>
2021-12-31 03:41:26 -0800

    edited the style for the website

--- src/components/projects/Sportfolio.js
+++ src/components/projects/Sportfolio.js
@@ -1,7 +1,7 @@
 const Sportfolio = () => {
   return (
     <div id='sportfolio'>
-      <img src='https://wallpaperaccess.com/full/4713424.jpg' alt='sportfolio'/>
+      <img src='https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/4k/original/15.jpg' alt='sportfolio'/>
       <div id='sportfolioInfo'>
         <h3>Sportfolio</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis pretium metus. Praesent mauris risus, luctus et laoreet sit amet.</p>
